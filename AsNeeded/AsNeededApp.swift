e35a4f5a51583e237c94140a09be87b86d1f4513
Dan Hart <dan-hart@users.noreply.github.com>
2022-09-26 18:24:32 -0500

    Initial commit

---
+++ AsNeeded/AsNeededApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsNeededApp.swift
+//  AsNeeded
+//
+//  Created by Dan Hart on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct AsNeededApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
