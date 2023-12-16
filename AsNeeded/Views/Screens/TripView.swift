c7fd4f82af04ce0257730727b2a9848228da9bdd
Dan Hart <dan-hart@users.noreply.github.com>
2023-12-16 12:01:41 -0600

    Implement trip planning

---
+++ AsNeeded/Views/Screens/TripView.swift
@@ -0,0 +1,18 @@
+//
+//  TripView.swift
+//  AsNeeded
+//
+//  Created by Dan Hart on 12/16/23.
+//
+
+import SwiftUI
+
+struct TripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripView()
+}
