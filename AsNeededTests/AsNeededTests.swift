7db887368c89178882e2cb3b22c1cc43c57f069a
Dan Hart <dan-hart@users.noreply.github.com>
2025-08-14 15:03:58 -0500

    Add Tests

---
+++ AsNeededTests/AsNeededTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsNeededTests.swift
+//  AsNeededTests
+//
+//  Created by Dan Hart on 8/14/25.
+//
+
+import Testing
+@testable import AsNeeded
+
+struct AsNeededTests {
+    @Test("quick test example")
+    func quickTestExample() {
+        let sum = 2 + 2
+        #expect(sum == 4)
+    }
+}
