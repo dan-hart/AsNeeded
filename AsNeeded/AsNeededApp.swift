d75050bed8e2ebcd6a971cfdb8becd060ffb34b3
Dan Hart <dan-hart@users.noreply.github.com>
2025-08-14 15:20:08 -0500

    Starting Fresh

--- AsNeeded/AsNeededApp.swift
+++ AsNeeded/AsNeededApp.swift
@@ -7,26 +7,12 @@
 
 import SwiftUI
 import AppIntents
-import SwiftData
-import SwiftDate
 
 @main
 struct AsNeededApp: App {
-    private var userData: UserData
-    
-    init() {
-        userData = UserData()
-        LogShortcuts.updateAppShortcutParameters()
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(userData)
-                .onDisappear {
-                    Logbook.shared.save()
-                }
         }
-        .modelContainer(Logbook.shared.sharedModelContainer)
     }
 }
