28fed85cd894d48184f63d0379a58d42a303a9f4
Dan Hart <dan-hart@users.noreply.github.com>
2025-09-06 10:29:56 -0500

    Package Restructuring

--- AsNeeded/Packages/SwiftRxNorm/Sources/SwiftRxNorm/RxNormInteraction.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// A representation of a drug-drug interaction result from RxNorm API.
-public struct RxNormInteraction: Codable, Hashable, Sendable {
-	public let description: String
-	public let drugs: [RxNormDrug]
-}
